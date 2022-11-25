e02b2ea3b23c93c006492a36fb2d7eb6244ab984
uziriahimu <uziriahimu@gmail.com>
2022-11-25 23:30:19 +0600

    logout

--- src/pages/Shared/Header.js
+++ src/pages/Shared/Header.js
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../../context/AuthProvider';
 
 const Header = () => {
+    const { user, logOut } = useContext(AuthContext)
+    const handleLogOut = () => {
+        logOut()
+            .then(() => { })
+            .catch(err => console.log(err))
+    }
 
     const menuItems = <React.Fragment>
         <li><Link to='/'>Home</Link></li>
         <li><Link to='/blog'>Blog</Link></li>
         <li><Link to='/category'>Category</Link></li>
-        <li><Link to='/login'>Login</Link></li>
+        {
+            user?.uid ?
+                <>
+                    <li><Link to='/dashboard'>Dashboard</Link></li>
+                    <li><button onClick={handleLogOut}>Sign Out</button> </li>
+                </>
+                :
+                <li><Link to='/login'>Login</Link></li>
+        }
+
 
     </React.Fragment>
 
